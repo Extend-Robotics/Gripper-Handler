15f320351e502579238802c86ad8198195a7cc76
VFjr <56606852+VFjr@users.noreply.github.com>
2020-08-10 15:54:24 +0100

    Add files via upload

---
+++ install_gripper_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/catkin_ws/src
+
+#dependencies:
+git clone https://github.com/ROBOTIS-GIT/DynamixelSDK
+git clone https://github.com/ROBOTIS-GIT/ROBOTIS-Framework
+git clone https://github.com/ROBOTIS-GIT/ROBOTIS-Framework-msgs
+git clone https://github.com/ROBOTIS-GIT/RH-P12-RN-A
